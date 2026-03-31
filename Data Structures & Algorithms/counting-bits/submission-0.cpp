7e541d4d6a28adf066f76081f5cd56e3ef94b3b0
peterprogaming <104953944+peterprogaming@users.noreply.github.com>
2026-03-31 16:40:13 +0700

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> res(n + 1, 0);
+
+        for (int i = 0; i <= n; ++i) res[i] = __builtin_popcount(i);
+        return res;
+    }
+};
